1797d2ccd4520679e10842cc6454a1964d39d1ba
Dhayalan <dhaijk01@in.ibm.com>
2019-09-07 17:10:07 +0530

    Updates

---
+++ precache-manifest.88050e6a137bd3ab759745ce32fd99ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20e43a29566c9d0718ca97704342c015",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "ddafc70b511d094eee6e",
+    "url": "/monster-rolodex/static/css/main.d4c4d115.chunk.css"
+  },
+  {
+    "revision": "01d8317fe3a3835ac7c6",
+    "url": "/monster-rolodex/static/js/2.f4bca13b.chunk.js"
+  },
+  {
+    "revision": "ddafc70b511d094eee6e",
+    "url": "/monster-rolodex/static/js/main.760e704c.chunk.js"
+  },
+  {
+    "revision": "384d151a98451564c735",
+    "url": "/monster-rolodex/static/js/runtime~main.50883f11.js"
+  }
+]);
